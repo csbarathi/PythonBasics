572317e4b96f92eebdcc05a842c23d3aa45fb727
CSB <csbarathi@gmail.com>
2022-05-10 15:28:26 +0000

    1

---
+++ DataTypes.py
@@ -0,0 +1,33 @@
+a = 5
+print("Type of a: ", type(a))
+  
+b = 5.0
+print("\nType of b: ", type(b))
+  
+c = 2 + 4j
+print("\nType of c: ", type(c))
+
+String1 = 'Hello World'
+print(String1) 
+print(type(String1))
+    
+String1 = "Hello's World"
+print(String1) 
+print(type(String1))
+ 
+String1 = '''Hello "world"'''
+print(String1) 
+print(type(String1))
+  
+String1 = '''Hello 
+            world'''
+print(String1) 
+print(type(String1))
+
+String1 = 'Hello World'
+print(String1[0])
+print(String1[1])
+print(String1[-1])
+ 
+List = [['Apple', 'Banana'], ['Carrot']] 
+print(List) 
